6ff713858ec1e0f5af6761ed2511453f7507d2a2
gabrielnarcizo <eligcan@hotmail.com>
2022-02-15 21:57:08 -0300

    chore: configuração de lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,38 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/airbnb'
+  extends: [
+    'plugin:vue/recomended',
+    '@vue/airbnb',
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'import/no-extraneous-dependencies': 'off',
+    'indent': ['error', 'tab'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-tabs': 'off',
+    'prefer-destructuring': ['error', {
+      'object': true,
+      'array': false,
+    }],
+    'vue/html-indent': ['error', 'tab'],
+    'vue/max-attributes-per-line': ['error', {
+      'singleLine': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }],
+    'vue/script-indent': ['error', 'tab', {
+      'baseIndent': 1,
+      'switchCase': 1,
+    }],
+    'vue/singleLine-html-element-content-newline': 'off',
+  },
+};
